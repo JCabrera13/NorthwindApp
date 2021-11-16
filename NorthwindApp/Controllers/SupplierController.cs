48c66d6e516b5b02691a153b9294e5b141a4f182
cabreramo <78823622+cabreramo@users.noreply.github.com>
2021-11-16 11:06:38 -0600

    Controller de supplier

---
+++ NorthwindApp/Controllers/SupplierController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NorthwindApp.Models;
+
+namespace NorthwindApp.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private readonly NorthwindContext db;
+
+        public SuppliersController(NorthwindContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            var listadoSuppliers = db.Suppliers.ToList();
+            return View();
+        }
+    }
+}
